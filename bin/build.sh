80e924ad70ce6b8006fd3f24c6fef1da3189e7b8
rickie120243 <rickie120243@gmail.com>
2016-10-14 17:24:54 +0800

    fix google drive folder id

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ELECTRON_VERSION=1.3.4
-FOLDER_ID=0B9GraSYa0W12cjJFNF
+FOLDER_ID=0B9GraSYa0W12cjJFNFR0MEJNZmM
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ZIP_IOS="sambotha-converter-darwin-x64-v${PACKAGE_VERSION}.zip"
 ZIP_WIN="sambotha-converter-win32-x64-v${PACKAGE_VERSION}.zip"
