8876978fa7db3161bb0c475725801fe90ddc4591
rickie120243 <rickie120243@gmail.com>
2016-10-14 17:01:26 +0800

    add app version & zip name

--- bin/build.sh
+++ bin/build.sh
@@ -2,6 +2,9 @@
 
 ELECTRON_VERSION=1.3.4
 FOLDER_ID=0B9GraSYa0W12cjJFNF
+PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
+ZIP_IOS="sambotha-converter-darwin-x64-v${PACKAGE_VERSION}.zip"
+ZIP_WIN="sambotha-converter-win32-x64-v${PACKAGE_VERSION}.zip"
 
 cd dist
 sed -i '' 's/http:\/\/localhost:8080/\./g' index.html
