8d78ad63601f7b7d8eacb4454480078b11cb0193
rickie120243 <rickie120243@gmail.com>
2016-10-14 17:02:18 +0800

    fix build process

--- bin/build.sh
+++ bin/build.sh
@@ -6,9 +6,10 @@ PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $
 ZIP_IOS="sambotha-converter-darwin-x64-v${PACKAGE_VERSION}.zip"
 ZIP_WIN="sambotha-converter-win32-x64-v${PACKAGE_VERSION}.zip"
 
+rm -rf dist
+webpack
+cp -r package.json main.js dist/
 cd dist
 sed -i '' 's/http:\/\/localhost:8080/\./g' index.html
-rm -rf sambotha-converter-*
-git clone git@github.com:rickie120243/pack-electron.git
-cp -r ./pack-electron/* ./
+
 electron-packager . sambotha-converter-v0.0.1 --platform=darwin,win32 --arch=x64 --overwrite
