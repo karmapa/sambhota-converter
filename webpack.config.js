edcdced760c509cfc5d36fb0515e3540125bdd57
rickie120243 <rickie120243@gmail.com>
2017-02-11 11:25:38 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,12 @@ var webpack = require('webpack');
 
 module.exports = {
 
-  devtool: 'eval',
+  devtool: 'source-map',
 
   entry: {
     javascript: [
+      'font-awesome-webpack!./assets/font-awesome.config.js',
+      'bootstrap-sass!./assets/bootstrap-sass.config.js',
       './src/index'
     ],
     html: './index.html'
@@ -26,21 +28,34 @@ module.exports = {
   },
 
   module: {
-    preLoaders: [
-      {
-        test: /\.js$/,
-        loader: 'eslint-loader',
-        configFile: './.eslintrc',
-        exclude: /node_modules/
-      }
-    ],
     loaders: [
+      {
+        test: /bootstrap\/js\//,
+        loader: 'imports?jQuery=jquery'
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         loaders: ['react-hot', 'babel'],
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader'},
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.scss$/,
+        loader: 'style!css!sass?outputStyle=expanded'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
+      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
+      {
+        test: /\.(png|jpg|cur)$/,
+        loader: 'url-loader?name=[path][name].[ext]&limit=8192'
+      },
       {
         test: /\.html$/,
         loader: "file-loader?name=[name].[ext]",
