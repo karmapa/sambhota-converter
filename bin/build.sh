7747d9fcfc97e40dbd52270de18b5c2b470c4378
chiehan1 <chieh.an.hsu@gmail.com>
2020-09-07 11:20:06 +0800

    fix ELECTRON_VERSION in build.sh

--- bin/build.sh
+++ bin/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ELECTRON_VERSION=1.3.4
+ELECTRON_VERSION=10.1.1
 FOLDER_ID=0B9GraSYa0W12cjJFNFR0MEJNZmM
 PACKAGE_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | xargs)
 ZIP_IOS="sambotha-converter-darwin-x64-v${PACKAGE_VERSION}.zip"
